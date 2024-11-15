9a7f9b8ba310e314599f2b23a3608471348c6c81
katoj65 <katoj652gmail.com>
2024-11-15 17:58:53 +0300

    Added carbon calculator overview screen

--- resources/js/Pages/CarbonCalculatorPage.vue
+++ resources/js/Pages/CarbonCalculatorPage.vue
@@ -16,64 +16,69 @@ In today’s world, every action we take whether it’s commuting, eating, or po
 Calculating your carbon emissions isn’t just about numbers; it’s about understanding your role in the fight against climate change and discovering opportunities to make a difference. Here’s why you should start today:
 </p>
 
-<ol>
-<li>
-<h6>
-Awareness is the First Step
-</h6>
-<p>
-You can’t manage what you don’t measure. Knowing the carbon emissions tied to your lifestyle whether it’s your commute, energy use, or dietary choices—shines a light on areas where you can reduce your environmental impact.
-</p>
-</li>
 
-<li>
-<h6>
-Combatting Climate Change Starts at Home
-</h6>
+
+
+
+<el-collapse accordion>
+<el-collapse-item name="1">
+<template slot="title">
+<h6>Awareness is the First Step</h6>
+</template>
+<p>You can’t manage what you don’t measure. Knowing the carbon emissions tied to your lifestyle whether it’s your commute, energy use, or dietary choices—shines a light on areas where you can reduce your environmental impact.</p>
+
+</el-collapse-item>
+<el-collapse-item name="2">
+<template slot="title">
+<h6>Combatting Climate Change Starts at Home </h6>
+</template>
 <p>
 Climate change may feel like a vast, global issue, but it’s driven by individual choices. By calculating your carbon footprint, you recognize how personal decisions contribute to a collective challenge, allowing you to take responsibility and be part of the solution.
 </p>
-</li>
-<li>
-<h6>
-Identify Cost-Effective Opportunities
-</h6>
+</el-collapse-item>
+<el-collapse-item name="3" slot="title">
+<template>
+<h6>Identify Cost-Effective Opportunities</h6>
+</template>
 <p>
- Many actions that reduce your carbon footprint like cutting energy waste, using efficient appliances, or driving less can also save you money. Measuring your emissions helps you identify where small adjustments can yield financial and environmental benefits.
+Many actions that reduce your carbon footprint like cutting energy waste, using efficient appliances, or driving less can also save you money. Measuring your emissions helps you identify where small adjustments can yield financial and environmental benefits.
 </p>
-</li>
-<li>
-<h6>Support Behavior Change</h6>
+
+</el-collapse-item>
+<el-collapse-item name="4">
+<template slot="title">
+<h6> Support Behavior Change</h6>
+</template>
 <p>
 By measuring and sharing your carbon footprint, you advocate for transparency and encourage businesses and governments to do the same. Your demand for greener options drives broader behavior change, fostering sustainable policies and innovations.
 </p>
-
-</li>
-<li>
-<h6> Track Your Progress</h6>
+</el-collapse-item>
+<el-collapse-item name="5">
+<template slot="title">
+<h6>Track Your Progress </h6>
+</template>
 <p>
 Climate action is a journey. Regularly measuring your carbon footprint helps you set realistic goals, monitor improvements, and celebrate milestones. It also keeps you motivated to keep striving for a more sustainable lifestyle.
 </p>
-</li>
-<li>
-<h6>Strengthen Community Impact</h6>
+</el-collapse-item>
+
+<el-collapse-item name="6">
+<template slot="title">
+<h6>Strengthen Community Impact </h6>
+</template>
 <p>
-    When you calculate your footprint, you’re not acting alone. Your efforts contribute to a growing global community taking action against climate change. Together, individual actions amplify into collective impact, fostering a sense of shared responsibility and accomplishment.
+When you calculate your footprint, you’re not acting alone. Your efforts contribute to a growing global community taking action against climate change. Together, individual actions amplify into collective impact, fostering a sense of shared responsibility and accomplishment.
 </p>
-</li>
-<li>
-<h6>Offset What You Can’t Avoid</h6>
+</el-collapse-item>
+<el-collapse-item name="7">
+<template slot="title">
+<h6>Offset What You Can’t Avoid </h6>
+</template>
 <p>
- While reducing emissions is vital, some activities will always produce carbon. Measuring your footprint helps you calculate exactly what you need to offset through credible carbon offset programs, allowing you to achieve net-zero emissions and support global sustainability projects.
+While reducing emissions is vital, some activities will always produce carbon. Measuring your footprint helps you calculate exactly what you need to offset through credible carbon offset programs, allowing you to achieve net-zero emissions and support global sustainability projects.
 </p>
-</li>
-
-</ol>
-
-
-
-
-
+</el-collapse-item>
+</el-collapse>
 
 
 </div>
